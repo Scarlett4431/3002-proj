b534f6669f4e01bba7271fb3afd6e03a7ba93e3f
Scarlett4431 <Scarlett4431@gmail.com>
2023-09-17 18:42:14 +0800

    add button for complete task

--- src/components/Board/Card.js
+++ src/components/Board/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import { XCircleIcon } from "@heroicons/react/24/solid";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
-function Card({ id, text, listID, index}) {
-
-  const handleDeleteCard= () => {
+function Card({ id, text, listID, index }) {
+  const handleDeleteCard = () => {
     // TODO
-  }
+  };
 
   return (
     <Draggable key={id} draggableId={id} index={index}>
@@ -20,9 +20,20 @@ function Card({ id, text, listID, index}) {
           >
             <div className="flex justify-between items-center p-5">
               <p>{text}</p>
-              <button onClick={handleDeleteCard} className="text-red-500 hover:text-red-600">
-                <XCircleIcon className="ml-5 h-8 w-8" />
-              </button>
+              <div className="flex items-end justify-end">
+                <button
+                  onClick={handleDeleteCard}
+                  className="hover:text-grey-900"
+                >
+                  <CheckCircleIcon className="h-8 w-8" />
+                </button>
+                <button
+                  onClick={handleDeleteCard}
+                  className="text-red-500 hover:text-red-600"
+                >
+                  <XCircleIcon className="h-8 w-8" />
+                </button>
+              </div>
             </div>
           </div>
         );
