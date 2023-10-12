de9e7ed99e75addd9a8d79c8bca3df7822061f2e
junya <junyanwu1996@gmail.com>
2023-10-12 20:58:57 +0800

    included in 3 steps guided tour

--- src/components/BoardCollection/BoardTour.js
+++ src/components/BoardCollection/BoardTour.js
@@ -3,53 +3,78 @@ import Shepherd from "shepherd.js";
 import "shepherd.js/dist/css/shepherd.css";
 
 const GuidedTour = () => {
-    const gifPath = process.env.PUBLIC_URL + '/assets/giphy.gif';
-  useEffect(() => {
-    const tour = new Shepherd.Tour({
-      defaultStepOptions: {
-        classes: "shepherd-theme-arrows",
-        when: {
-            show: function() {
-                var gifContainer = document.createElement('div');
-                gifContainer.style.backgroundImage = `url(${gifPath})`;
-                gifContainer.style.backgroundSize = 'cover';
-                gifContainer.style.width = '100%';
-                gifContainer.style.height = '400px'; // Set height according to your needs
-                this.el.querySelector('.shepherd-text').appendChild(gifContainer);
+    const gifPaths = [
+        process.env.PUBLIC_URL + '/assets/giphy1.gif',
+        process.env.PUBLIC_URL + '/assets/giphy2.gif',
+        process.env.PUBLIC_URL + '/assets/giphy3.gif'
+        
+    ];
+
+    useEffect(() => {
+        const tour = new Shepherd.Tour({
+            defaultStepOptions: {
+                classes: "shepherd-theme-arrows",
+                when: {
+                    show: function() {
+                        const stepIndex = tour.steps.indexOf(this);
+                        var gifContainer = document.createElement('div');
+                        gifContainer.style.backgroundImage = `url(${gifPaths[stepIndex]})`;
+                        gifContainer.style.backgroundSize = 'cover';
+                        gifContainer.style.width = '100%';
+                        gifContainer.style.height = '400px'; // Set height according to your needs
+                        this.el.querySelector('.shepherd-text').appendChild(gifContainer);
+                    }
+                }
             }
-        }
-      }
-    });
+        });
 
+        const stepButtons = [
+            [
+                {
+                    text: "Next",
+                    action: tour.next
+                }
+            ],
+            [
+                {
+                    text: "Previous",
+                    action: tour.back,
+                    classes: "shepherd-button-secondary"
+                },
+                {
+                    text: "Next",
+                    action: tour.next
+                }
+            ],
+            [
+                {
+                    text: "End",
+                    action: tour.next
+                }
+            ],
+        ];
 
+        // Add steps with different GIFs
+        gifPaths.forEach((path, index) => {
+            tour.addStep({
+                id: `step${index + 1}`,
+                text: `Welcome to the guided tour! This is step ${index + 1}.`,
+                attachTo: {
+                    element: ".element-selector",
+                    on: "bottom"
+                },
+                buttons: stepButtons[index]
+            });
+        });
 
+        tour.start();
 
+        return () => {
+            tour.complete();
+        };
+    }, []); // Empty dependency array ensures the effect runs once after the initial render
 
-    tour.addStep({
-      id: "step1",
-      text: "Welcome to the guided tour! This is step 1.",
-      attachTo: {
-        element: ".element-selector",
-        on: "bottom"
-      },
-      buttons: [
-        {
-          text: "Next",
-          action: tour.next
-        }
-      ]
-    });
-
-    // Add more steps as needed
-
-    tour.start();
-
-    return () => {
-      tour.complete();
-    };
-  }, []); // Empty dependency array ensures the effect runs once after the initial render
-
-  return null; // GuidedTour component doesn't render anything, it's just for handling the tour logic
+    return null;
 };
 
 export default GuidedTour;
